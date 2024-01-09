ff66c4c5bf7353e09500753c561e9354234f1255
pratikjadhav123 <118898674+pratikjadhav123@users.noreply.github.com>
2024-01-09 20:59:23 +0530

    Initial commit

---
+++ StudentApplication_ASPWEBAPI/Model/StudentDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentApplication_ASPWEBAPI.Model
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Student { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=LAPTOP-4N34TBLJ\\SQLEXPRESS;Initial Catalog=Student_Details;Integrated Security=True;Trust Server Certificate=True");
+        }
+    }
+}
